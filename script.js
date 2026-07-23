3f99090fae631d94d3e48713c8bd437a242c88c3
saritagabi329-svg <saritagabi329@gmail.com>
2026-07-23 08:05:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ btnReportar.addEventListener('click', () => {
 btnManual.addEventListener('click', () => {
     alert("Seguro qué quieres leer 60 páginas de manual?");
 });     
+// 1. Seleccionamos el botón y el menú nav
+const botonMenu = document.querySelector('#boton-menu');
+const menuNavegacion = document.querySelector('#navegacion');
+
+// 2. Escuchamos el clic en el botón hamburguesa
+botonMenu.addEventListener('click', () => {
+    // 3. El toggle quita o pone la clase "menu-oculto" dinámicamente
+    menuNavegacion.classList.toggle('menu-oculto');
+});
